e56091bb9adace4a4965788aef79062c72ee4d89
Test <Test>
2020-11-13 14:47:44 +0300

    HeplPlease

---
+++ TestProject/CustomHeaderView.swift
@@ -0,0 +1,22 @@
+//
+//  CustomHeaderView.swift
+//  TestProject
+//
+//  Created by user on 13/11/2020.
+//
+
+import UIKit
+
+class CustomHeaderView: UITableViewHeaderFooterView {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- TestProject/ViewController.swift
+++ TestProject/ViewController.swift
@@ -7,13 +7,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+        override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
+    // Mark: UITableViewDelegate, UITableViewDataSource
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Section: \(indexPath.section), row: \(indexPath.row)"
+        return cell
+    }
+    
+    let headerID = String(describing: CustomHeaderView.self)
+    
+    private func tableViewConfig() {
+        let nib = UINib(nibName: headerID, bundle: nil)
+        tableView.register(nib, forHeaderFooterViewReuseIdentifier: headerID)
+        
+        tableView.tableFooterView = UIView()
+            }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let header = tableView.dequeueReusableHeaderFooterView(withIdentifier: headerID) as!CustomHeaderView
+        return header
+    }
+    
+    func tableView(_tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 60
+    }
+    }
+    
 
-
-}
 
